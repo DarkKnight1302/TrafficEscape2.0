e861d3ccaed3b0755475e34066e69a7f5b4bcea1
ROBIN PARASHAR <robin.parashar@yahoo.in>
2025-07-21 17:55:11 +0530

    Add try - catch

--- TrafficEscape2.0/ApiClients/GoogleTrafficApiClient.cs
+++ TrafficEscape2.0/ApiClients/GoogleTrafficApiClient.cs
@@ -33,10 +33,17 @@ namespace TrafficEscape2._0.ApiClients
             HttpResponseMessage response = await httpClient.GetAsync($"https://maps.googleapis.com/maps/api/distancematrix/json?destinations=place_id:{toPlaceId}&origins=place_id:{fromPlaceId}&key={ApiKey}&departure_time=now");
             if (response.IsSuccessStatusCode)
             {
-                var result = await response.Content.ReadAsStringAsync();
-                DistanceApiResponse apiResponse = JsonConvert.DeserializeObject<DistanceApiResponse>(result);
-                int trafficTime = apiResponse?.rows[0]?.elements[0]?.duration_in_traffic.value ?? -1;
-                return (int)(trafficTime / 60);
+                try
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    this.logger.LogInformation($"Response from google distance API {result}");
+                    DistanceApiResponse apiResponse = JsonConvert.DeserializeObject<DistanceApiResponse>(result);
+                    int trafficTime = apiResponse?.rows[0]?.elements[0]?.duration_in_traffic.value ?? -1;
+                    return (int)(trafficTime / 60);
+                } catch (Exception e)
+                {
+                    this.logger.LogError(e.Message);
+                }
             }
             return -1;
         }
